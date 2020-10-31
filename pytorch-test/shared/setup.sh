2b67e43f881cc7152a675617d31095efaf56ddd5
Choke222 <chokechoke1@gmail.com>
2020-10-31 14:23:08 +0900

    gitignoreファイルを編集

--- pytorch-test/shared/setup.sh
+++ pytorch-test/shared/setup.sh
@@ -21,3 +21,4 @@ sudo systemctl list-unit-files | grep firewalld
 # pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 # pip install torch==1.4.0+cpu
 # pip install torchvision==0.5.0+cpu
+#meno
