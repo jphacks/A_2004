5eeed17fdfa57954e26af5cc3abb6e1291b196ef
Choke222 <chokechoke1@gmail.com>
2020-11-02 01:30:20 +0900

    DCGANの動作確認が完了＋GANに関する参考記事のメモを追加

--- deep/setup_local.sh
+++ deep/setup_local.sh
@@ -1,18 +1,18 @@
 sudo apt install python3-pip
 sudo apt install python-pip
 sudo apt update
-sudo pip install --upgrade pip
-pip install numpy
-pip install Click==7.0
-pip install Flask==1.1.1
-pip install itsdangerous==1.1.0
-pip install Jinja2==2.10.3
-pip install MarkupSafe==1.1.1
-pip install Werkzeug==0.16.0
-pip install gunicorn==19.10.0
-pip install torch==1.4.0 torchvision==0.5.0 -f https://download.pytorch.org/whl/torch_stable.html
-pip install flask
-pip install future --upgrade
+sudo pip3 install --upgrade pip3
+pip3 install numpy
+pip3 install Click==7.0
+pip3 install Flask==1.1.1
+pip3 install itsdangerous==1.1.0
+pip3 install Jinja2==2.10.3
+pip3 install MarkupSafe==1.1.1
+pip3 install Werkzeug==0.16.0
+pip3 install gunicorn==19.10.0
+pip3 install torch==1.4.0 torchvision==0.5.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install flask
+pip3 install future --upgrade
 # 通信の設定 
 sudo systemctl stop firewalld.service
 sudo systemctl mask firewalld.service
