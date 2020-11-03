ae7f156c7c98e73ed0603711212a4e28aa22c733
Choke222 <chokechoke1@gmail.com>
2020-11-03 18:13:40 +0900

    学習済みモデルを読み込み画像を生成するプログラムの準備ファイルを追加

--- pytorch-test/shared/deep/setup_added.sh
+++ pytorch-test/shared/deep/setup_added.sh
@@ -2,3 +2,6 @@ pip install tqdm
 pip3 install tqdm
 pip install statistics
 pip3 install statistics
+pip install networks
+pip3 install networks
+pip3 install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
